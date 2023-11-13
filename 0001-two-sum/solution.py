7e8c2189ece0e7ad8a609655424d3aa42ed1d7b4
Adarsh Anil <156306977+adarshanil07@users.noreply.github.com>
2023-11-13 16:02:53 +0000

    [LeetCode Sync] Runtime - 2313 ms (9.86%), Memory - 14.1 MB (17.76%)

---
+++ 0001-two-sum/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for x in range(len(nums)):
+            for y in range(x+1,len(nums)):
+                if target == (nums[x] + nums[y]):
+                    return (x,y)
+                
+        
+        
