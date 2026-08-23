4fecd985987a646e262cee57dac955f9e59b5338
Adarsh Anil <156306977+adarshanil07@users.noreply.github.com>
2026-08-23 12:48:14 +0000

    [LeetCode Sync] Runtime - 13 ms (43.30%), Memory - 12.2 MB (90.72%)

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def isPalindrome(self, x):
-        num_string = str(x)
-        reversed_string = self.reverse_string(num_string)
-        
-        if num_string == reversed_string:
-            return True
-        else:
-            return False
-    
-    def reverse_string(self, string):
-        stack = []
-        for element in string:
-            stack.append(element)
-            
-        reversed_string = ""
-        while stack:
-            reversed_string += stack.pop()
-        return reversed_string
-  
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        num = str(x)
+        numLen = len(num)
+        palindrome = True
+
+        for i in range (0, numLen):
+            if num[i] != num[numLen - 1 - i]:
+                return False
+        return True
+                
+
+
         
